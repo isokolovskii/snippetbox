1bb6323d8b524ac891efd6c1f4fc312aac699e69
Sokolovskii Ivan <ivan3177@gmail.com>
2025-11-25 21:59:20 +0300

    feat(http): middleware for common http headers

---
+++ cmd/web/middleware.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+)
+
+func commonHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Set("Content-Security-Policy",
+			"default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
+
+		writer.Header().Set("Referrer-Policy", "origin-when-cross-origin")
+		writer.Header().Set("X-Content-Type-Options", "nosniff")
+		writer.Header().Set("X-Frame-Options", "deny")
+		writer.Header().Set("X-XSS-Protection", "0")
+
+		writer.Header().Set("Server", "Go")
+
+		next.ServeHTTP(writer, request)
+	})
+}
