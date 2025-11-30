2a353efd269ddac3a57426ae8cabc6339a02b970
Sokolovskii Ivan <ivan3177@gmail.com>
2025-11-30 14:08:45 +0300

    feat(auth): migration to add users table to database

--- db/init/01.sql
+++ db/init/01.sql
@@ -4,4 +4,4 @@ CREATE DATABASE snippetbox CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Create user for app --
 CREATE USER 'web'@'%' IDENTIFIED WITH mysql_native_password BY 'pass';
 -- Grant privileges for user to manipulate database --
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, INDEX, ALTER ON snippetbox.* TO 'web'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, INDEX, ALTER, DROP ON snippetbox.* TO 'web'@'%';
